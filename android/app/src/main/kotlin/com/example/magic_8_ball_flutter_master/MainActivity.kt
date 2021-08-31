62df084108d5416b1fbd06056701acd55b388249
sudharaD <sudharad7@gmail.com>
2021-09-01 00:15:58 +0530

    App started

---
+++ android/app/src/main/kotlin/com/example/magic_8_ball_flutter_master/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.magic_8_ball_flutter_master
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: BallPage(),
+    ),
+  );
+}
+
+class BallPage extends StatelessWidget {
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Ball();
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
